3865c449f375e3c1f098b9c8a96c8c06785a423a
Grecia Escárcega Maldonado <42050505+GreciaEM@users.noreply.github.com>
2020-09-14 13:02:26 -0500

    Project creation

---
+++ SimpsonsApp/SimpsonsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpsonsApp
+//
+//  Created by Grecia Escárcega on 14/09/20.
+//  Copyright © 2020 GEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
